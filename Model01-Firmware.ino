144cc66f67b002b4caabca1b907d725fa9e1f9b8
Dennis Lambe Jr <malsyned@malsyned.net>
2017-11-08 16:37:33 -0500

    Moved ctrl, alt, and shift mostly back to factory positions

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -131,14 +131,14 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftShift, Key_Backspace, Key_LeftControl, Key_LeftAlt,
+   Key_LeftControl, Key_Backspace, Key_LeftAlt, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
    Key_RightAlt,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_KeypadNumLock,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_LeftGui,   Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_LeftAlt, Key_RightControl, Key_Spacebar, Key_RightShift,
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] =  KEYMAP_STACKED
